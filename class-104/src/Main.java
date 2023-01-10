3b80758b8f89fcc9311a411495966fd902c6568a
Jaume Sánchez <jasato89@gmail.com>
2023-01-10 11:53:11 +0100

    added filewriter

--- class-104/src/Main.java
+++ class-104/src/Main.java
@@ -1,8 +1,10 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-      
+    public static void main(String[] args) throws IOException {
+
+/*
 
         String[] names = {"Jaume", "Alejandro"};
 
@@ -21,6 +23,7 @@ public class Main {
         System.out.println("Por favor, introduce tu edad:");
 
         int age = input.nextInt();
+        input.nextLine();
 
         if (age >= 18) {
             System.out.println("Eres mayor de edad");
@@ -33,7 +36,7 @@ public class Main {
         System.out.println("1. Sumar");
         System.out.println("2. Restar");
 
-        int selection = input.nextInt();
+        int selection = Integer.parseInt(input.nextLine());
 
         if (selection == 1) {
             System.out.println("2 + 2 = 4");
@@ -43,7 +46,65 @@ public class Main {
             System.err.println("Comando no válido");
         }
 
+        System.out.println("Introduce la frase");
+
+        String phrase = input.nextLine();
         input.close();
+
+        int totalwords = startsWithVowel(phrase);
+
+ */
+
+        FileWriter writer = new FileWriter("/Users/jasato/Desktop/IH-Java-11.nosync/demo.txt", true);
+        writer.write("Hola clase \n");
+        writer.close();
+
+        File archivo = new File("/Users/jasato/Desktop/IH-Java-11.nosync/demo.txt");
+
+        Scanner fileReader = new Scanner(archivo);
+
+        while (fileReader.hasNextLine()) {
+            System.out.println(fileReader.nextLine());
+        }
+
+        fileReader.close();
+
+        File oscars = new File("oscar_age_female.csv");
+
+        Scanner oscarsReader = new Scanner(oscars);
+
+        FileWriter oscarsWritter = new FileWriter("result.txt");
+
+        while (oscarsReader.hasNextLine()) {
+            String[] line = oscarsReader.nextLine().split(", ");
+            oscarsWritter.write("Name: " + line[3]);
+            oscarsWritter.write("Year: " + line[1]);
+            oscarsWritter.write("Age: " + line[2]);
+
+        }
+
+
+
+
+
+
+    }
+
+    public static int startsWithVowel(String phrase) {
+        String[] words = phrase.toLowerCase().split(" ");
+
+        int counter = 0;
+        for (String word : words) {
+           // char c = word.charAt(0);
+            if (word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u")) {
+                counter++;
+            } else {
+                System.err.println("La palabra " + word+ " no empieza por vocal");
+            }
+        }
+
+        System.out.println("Número total de palabras que empiezan por vocal: " + counter);
+        return counter;
 
     }
 }
