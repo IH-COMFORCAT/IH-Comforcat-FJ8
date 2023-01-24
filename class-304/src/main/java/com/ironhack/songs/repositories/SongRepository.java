fa468271cd5d00b1ebe05ee2378de0274a03a71b
Jaume Sánchez <jasato89@gmail.com>
2023-01-24 13:35:03 +0100

    jpa queries

--- class-304/src/main/java/com/ironhack/songs/repositories/SongRepository.java
+++ class-304/src/main/java/com/ironhack/songs/repositories/SongRepository.java
@@ -4,6 +4,14 @@ import com.ironhack.songs.models.*;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
+import java.util.*;
+
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
+
+    //SELECT * from song where artist = "ACDC"
+    List<Song> findByArtistContaining(String artist);
+    List<Song> findByArtistAndAlbumTitle(String artist, String albumTitle);
+    Song findBySongTitle(String songTitle);
+
 }
--- class-304/src/test/java/com/ironhack/songs/repositories/SongRepositoryTest.java
+++ class-304/src/test/java/com/ironhack/songs/repositories/SongRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -13,13 +15,19 @@ public class SongRepositoryTest {
     @Autowired
     SongRepository songRepository;
     Song song;
+    Song song2;
+    Song song3;
+    Song song4;
 
     @BeforeEach
     void setUp() {
         song = new Song("Back in black", "Back in black", "ACDC");
-        System.out.println(song.getId());
-        song = songRepository.save(song);
-        System.out.println(song.getId());
+        song2 = new Song("Thunderstruck", "Back in black", "ACDC");
+        song3 = new Song("I will possess your Heart", "DC", "Death Cab for Cutie");
+        song4 = new Song("I am the antichrist", "String", "Kishie Bashie");
+
+        songRepository.saveAll(List.of(song, song2, song3, song4));
+
 
     }
 
@@ -31,8 +39,24 @@ public class SongRepositoryTest {
 
     @Test
     void shouldAddNewSong_WhenMethodSaveCalled() {
+        Optional<Song> newSongOptional = songRepository.findById(8l);
+
+        if (newSongOptional.isPresent()) {
+            Song  song = newSongOptional.get();
+        }
         assertEquals("Back in black", songRepository.findById(song.getId()).get().getSongTitle());
         assertEquals(1, songRepository.findAll().size());
+
+    }
+
+    @Test
+    void shouldFindSongsByArtist_WhenArtistIsGiven() {
+        List<Song> acdcSongs = songRepository.findByArtistContaining("AC");
+        assertEquals(2, acdcSongs.size());
+
+        System.out.println(acdcSongs.get(0).getSongTitle());
+
+        //Song mySong = songRepository.findBySongTitle("Back in Black");
 
     }
 
